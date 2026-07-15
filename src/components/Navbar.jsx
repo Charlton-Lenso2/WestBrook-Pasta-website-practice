1208d1100234fb5733f5faf19b042fb17d6575ff
Charlton-Lenso2 <lensocharlton2@gmail.com>
2026-07-15 08:24:10 +0200

    Making it mobile responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,50 @@
-import {Link} from "react-router-dom";
-import "./Navbar.css"
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import "./Navbar.css";
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const closeMenu = () => setIsOpen(false);
+
   return (
-    <>
-      <div className="navbar">
-        <div className="navsection">
-          <h1 className="name">WestBrook Pasta</h1>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/food" >Food</Link>
-            </li>
-            <li>
-              <Link to="/about" >About</Link>
-            </li>
-            <li>
-              <Link to="/contact" className="contact-pill">
-                Contact
-              </Link>
-            </li>
-          </ul>
-        </div>
+    <div className="navbar">
+      <div className="navsection">
+        <h1 className="name">WestBrook Pasta</h1>
+
+        <button
+          className="hamburger"
+          onClick={() => setIsOpen(!isOpen)}
+          aria-label="Toggle menu"
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+
+        <ul className={isOpen ? "nav-links open" : "nav-links"}>
+          <li>
+            <Link to="/" onClick={closeMenu}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/food" onClick={closeMenu}>
+              Food
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" onClick={closeMenu}>
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact" className="contact-pill" onClick={closeMenu}>
+              Contact
+            </Link>
+          </li>
+        </ul>
       </div>
-    </>
+    </div>
   );
 }
 
